30b54d83483858bf44836f622209de37f3bc6558
Stefano Masini <stefano@stefanomasini.com>
2016-04-21 22:38:28 +0200

    more work on raspi

--- raspi/js/entrypoint_arduino.js
+++ raspi/js/entrypoint_arduino.js
@@ -11,28 +11,27 @@ scrollingText.setText('vrijeschool mareland');
 
 runApps([
     // speedBars,
-    new OscilatingBar(matrix.submatrix([0,1,2,3,4], range(0, matrix.numCols))),
-    //scrollingText,
+    new OscilatingBar(matrix.submatrix([0,1,9], range(0, matrix.numCols))),
+    scrollingText,
 ]);
 
 
 
-var NUM_LEDS = 150;
+var NUM_LEDS = 300;
 var BATCH_SIZE = 30;
 var i = 0;
+var matrixData = null;
 
 function generateCommand() {
     if (i*BATCH_SIZE < NUM_LEDS) {
+        if (matrixData === null) {
+            matrixData = matrix.rows.map(row => row.slice(0));
+        }
         var letter = String.fromCharCode(65+i);
         var chunks = [];
         for (var idx=0; idx<BATCH_SIZE; idx += 1) {
             var led;
-            if (idx % 2 === 0) {
-                led = matrix.rows[i][idx];
-            } else {
-                // Odd rows are backwards
-                led = matrix.rows[i][BATCH_SIZE-1-idx];
-            }
+            led = matrixData[i][idx];
             chunks.push(String.fromCharCode(led.r * 63 + 33));
             chunks.push(String.fromCharCode(led.g * 63 + 33));
             chunks.push(String.fromCharCode(led.b * 63 + 33));
@@ -42,38 +41,35 @@ function generateCommand() {
         return cmd;
     } else {
         i = 0;
+        matrixData = null;
         return 'B';
     }
 }
 
 
-class SerialAdapter {
-    constructor(device, baudrate) {
-        this.serialPort = new SerialPort(device, { baudrate: baudrate });
-        this.ready = false;
-        this.serialPort.on('open', () => {
-            console.log('Serial port ready');
-            this.ready = true;
-            this.serialPort.on('data', data => {
-                // console.log('data received: ' + data);
-                data = `${data}`.trim();
-                if (data.startsWith('OK')) {
-                    var data = generateCommand();
-                    this.serialPort.write(data+'\n', (err, results) => {
-                        if (err) {
-                            console.log('err ' + err);
-                        // } else {
-                        //     console.log('results ' + results);
-                        }
-                    });
-                }
-            });
-        });
+function handleSerialData(data, writeToSerial) {
+    if (data.startsWith('OK')) {
+        writeToSerial(generateCommand());
     }
 }
 
-var serialAdapter = new SerialAdapter('/dev/ttyUSB0', 115200)
 
+function runSerialPort(device, baudrate) {
+    var serialPort = new SerialPort(device, { baudrate: baudrate });
+    serialPort.on('open', () => {
+        console.log('Serial port open');
+    });
+    serialPort.on('data', data => {
+        data = `${data}`.trim();
+        handleSerialData(data, (dataToWrite) => {
+            serialPort.write(dataToWrite+'\n', (err, results) => {
+                if (err) {
+                    console.log('Error writing data: ' + err.message);
+                }
+            });
+        });
+    });
+}
 
-
+runSerialPort('/dev/ttyUSB0', 115200);
 
