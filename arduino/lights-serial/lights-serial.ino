dbe5fa0e6e8e18826fd481df9b09e0a4f6d905c4
Stefano Masini <stefano@stefanomasini.com>
2016-04-21 22:36:06 +0200

    more work on arduino

--- arduino/lights-serial/lights-serial.ino
+++ arduino/lights-serial/lights-serial.ino
@@ -1,24 +1,16 @@
 #include <Adafruit_NeoPixel.h>
 
 #define LED_STRIP_PIN         6
-
-#define NUM_PIXELS_PER_STRIP  150
-
-#define BATCH_LENGTH  30
+#define NUM_PIXELS_PER_STRIP  300
+#define BATCH_LENGTH          30
+#define LAST_BATCH_IDX        (NUM_PIXELS_PER_STRIP/BATCH_LENGTH)-1
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_PIXELS_PER_STRIP, LED_STRIP_PIN, NEO_GRB + NEO_KHZ800);
 
-
 void setup() {
   Serial.begin(115200);
-
   pixels.begin();
-
   turnOffLedStrips();
-
-//  Serial.println("Started");
-//  Serial.println("OK");
-
 }
 
 int parseChar(char chVal) {
@@ -30,35 +22,41 @@ int parseChar(char chVal) {
   return val;
 }
 
-String str;
-
 void turnOffLedStrips() {
   for (int i=0; i<NUM_PIXELS_PER_STRIP; i+=1) {
     pixels.setPixelColor(i, pixels.Color(0, 0, 0));
   }
   pixels.show();
-//  delay(50);
   Serial.println("OK Reset");
 }
 
+String str;
+
 void driveLedStrip() {
-    int batchIdx = constrain((char)(str.charAt(1)) - 65, 0, (NUM_PIXELS_PER_STRIP/BATCH_LENGTH)-1);
-    int numTriplesInCommand = (str.length()-2) / 3;
-    int numLEDsToSet = min(numTriplesInCommand, BATCH_LENGTH);
-    int bufIdx = 2;
-    int r, g, b;
-    for (int i=0; i<numTriplesInCommand; i+=1) {
-      r = parseChar(str.charAt(bufIdx++));
-      g = parseChar(str.charAt(bufIdx++));
-      b = parseChar(str.charAt(bufIdx++));
-      pixels.setPixelColor(i + batchIdx*BATCH_LENGTH, pixels.Color(r, g, b));
-    }
-    Serial.print("OK set ");
-    Serial.print(numLEDsToSet);
-    Serial.print(" LEDs in batch ");
-    Serial.println(batchIdx);
-//    pixels->show();
-//    delay(50);
+  // pos 1: batch idx (starting from "A", ASCII 65)
+  // following triples of RGB values
+  int batchIdx = constrain((char)(str.charAt(1)) - 65, 0, LAST_BATCH_IDX);
+  int numTriplesInCommand = (str.length()-2) / 3;
+  int numLEDsToSet = min(numTriplesInCommand, BATCH_LENGTH);
+  int bufIdx = 2;
+  int r, g, b;
+  for (int i=0; i<numLEDsToSet; i+=1) {
+    r = parseChar(str.charAt(bufIdx++));
+    g = parseChar(str.charAt(bufIdx++));
+    b = parseChar(str.charAt(bufIdx++));
+    // Odd rows are indexed backwards because they're chained in zig-zag
+    int pixelIdx = (batchIdx % 2 == 0) ? i : (BATCH_LENGTH-1-i);
+    pixels.setPixelColor(pixelIdx + batchIdx*BATCH_LENGTH, pixels.Color(r, g, b));
+  }
+  Serial.print("OK set ");
+  Serial.print(numLEDsToSet);
+  Serial.print(" LEDs in batch ");
+  Serial.println(batchIdx);
+}
+
+void showPixels() {
+  pixels.show();
+  Serial.println("OK Show");
 }
 
 
@@ -66,20 +64,19 @@ void loop() {
   char command;
   while (Serial.available() > 0) {
     str = Serial.readStringUntil('\n');
+    // pos 0: command
     command = str.charAt(0);
 
     if (command == 'A') {
       driveLedStrip();
     } else if (command == 'B') {
-      pixels.show();
-      Serial.println("OK Show");
+      showPixels();
     } else if (command == '0') {
       turnOffLedStrips();
     } else {
       Serial.print("ERROR Unknown command: ");
       Serial.println(command);
     }
-//    Serial.println("OK");
   }
 }
 
